5e47d7283a87252bc764628dd647990c1f149feb
Guilherme Dias <gmd.delta@gmail.com>
2021-07-05 15:49:55 -0300

    [PT-BR] Resolvido requisito 9. [EN] Solved 9th requirement.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from './components/Home';
 import About from './components/About';
 import Users from './components/Users';
-import { BrowserRouter, Link, Route} from 'react-router-dom'
+import { BrowserRouter, Link, Route, Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -11,9 +11,11 @@ class App extends Component {
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/users">Users</Link>
-        <Route  exact path ="/" component={Home} />
-        <Route path ="/about" component={About} />
-        <Route path ="/users" render={ () => <Users greetingsMessage="Good Morning" /> } />
+        <Switch>
+          <Route  exact path ="/" component={Home} />
+          <Route path ="/about" component={About} />
+          <Route path ="/users/:id" render={ (props) => <Users {...props} greetingsMessage="Good Morning" /> } />
+        </Switch>
       </BrowserRouter>
     );
   }
